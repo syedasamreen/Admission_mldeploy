da4368be48ea82c79f4700e767461ad5a079fb95
syedasamreen <syedasamreen.saheb@gmail.com>
2022-08-06 20:57:42 +0530

    make it better

--- app.py
+++ app.py
@@ -45,5 +45,5 @@ def index():
 
 
 if __name__ == "__main__":
-    #app.run(host='127.0.0.1', port=8001, debug=True)
+    # app.run(host='127.0.0.1', port=8001, debug=True)
 	app.run(debug=True) # running the app
